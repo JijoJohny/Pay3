b0e0a3021338656f91e49ccc85a7be0853901238
JijoJohny <147225080+JijoJohny@users.noreply.github.com>
2024-09-09 12:12:37 +0530

    First commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async(req,res)=>{
+    res.json({
+        data:"you  hit the signup button",
+    });
+};
+
+export const login = async(req,res)=>{
+    res.json({
+        data:"you  hit the login button",
+    });
+};
+
+export const logout = async(req,res)=>{
+    res.json({
+        data:"you  hit the logout button",
+    });
+};
